3cda0f1bb13786381aa618db42193064f2dffc88
Ever Blua <everblua@gmail.com>
2023-08-12 19:59:58 -0300

    NavigationBar in Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { lazy } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import NavigationBar from './NavigationBar';
 import Layout from './layout/Layout';
 
 const WalletList = lazy(() => import('./wallets/WalletList'));
@@ -13,7 +12,6 @@ const NotFound = lazy(() => import('./common/NotFound'));
 function App() {
   return (
     <BrowserRouter>
-      <NavigationBar />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="wallets" element={<WalletList />} />
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,16 +3,17 @@ import { Box } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import NavigationBar from '../NavigationBar';
 import Loading from '../common/Loading';
 
 const Layout = () => {
   return (
     <Box
-      // display="flex"
       justifyContent="center"
       alignItems="center"
       height="300px"
     >
+      <NavigationBar />
       <Grid container spacing={4}>
         <Suspense fallback={<Loading plain />}>
           <Outlet />
